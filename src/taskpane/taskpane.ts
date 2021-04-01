2909ada0c891cbd178176d8b6b1e1cfd906b6ba9
Ben Ridgway <benridgway83@gmail.com>
2021-04-01 22:44:25 +0100

    second commit

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -12,24 +12,46 @@ import "../../assets/icon-80.png";
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported('WordApi', '1.3')) {
+      console.log('Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.');
+    }
+
+    // Assign event handlers and other initialization logic.
+    document.getElementById("insert-paragraph").onclick = insertParagraph;
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    //document.getElementById("run").onclick = run;
   }
 });
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
-    await context.sync();
+function insertParagraph() {
+  Word.run(function (context) {
+    var docBody = context.document.body;
+    docBody.insertParagraph("Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+                            "Start");
+    return context.sync();
+  })
+  .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
   });
 }
+
+// export async function run() {
+//   return Word.run(async (context) => {
+//     /**
+//      * Insert your Word code here
+//      */
+
+//     // insert a paragraph at the end of the document.
+//     const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+//     // change the paragraph color to blue.
+//     paragraph.font.color = "blue";
+
+//     await context.sync();
+//   });
+// }
